fc90db989cd6f06dc2638f7b96cf67a459b40471
Marco Dalla Stella <marco@dallastella.name>
2023-12-03 21:35:03 +0100

    [2023] Day 1 Part 1

--- src/solutions/2023/day01.clj
+++ src/solutions/2023/day01.clj
@@ -7,21 +7,62 @@
             [clojure.string :as str]))
 
 ;; # Problem
-{:nextjournal.clerk/visibility {:code :hide :result :show}}
+
 (clerk/html (u/load-problem "01" "2023"))
-{:nextjournal.clerk/visibility {:code :show :result :show}}
 
 ;; # Solution
 ;;
 ;; First things first, let's load our input and parse it
+{:nextjournal.clerk/visibility {:result :hide}}
 (def input (->> (slurp (io/resource "inputs/2023/day01.txt")) ;; Load the resource
                 str/split-lines))                             ;; Split into lines
-{:nextjournal.clerk/visibility {:result :hide}}
+{:nextjournal.clerk/visibility {:code :show :result :show}}
 
 ;; ## Part 1
+
+;; For this problem it would be easier to use regexp but, since they can be
+;; pretty slow, I'm going to use a different approach.
+;;
+;; String is a sequence of characters and every character has a numeric
+;; value (ex: (int \a) = 97). Let's use this to collect all the digits in a
+;; line.
+
+;; Let's define a set with all the digits value
+(def numbers-range (set (range (int \0) (inc (int \9)))))
+
+;; Sets can be used as a function to check if an element is present in the set.
+(defn numerical-char?
+  [c]
+  (not (nil? (numbers-range (int c)))))
+
+;; Let try it with "1":
+(numerical-char? \1)
+
+;; and with "A"
+(numerical-char? \A)
+
+;; We can now define a function that filters the digits from a string, keeps
+;; the first and the last in a vector, and then transform them in a number:
+(defn collect-numbers
+  [s]
+  (->> s
+       (filter numerical-char?)
+       ((juxt first last))
+       (apply str)
+       Integer/parseInt))
+
+;; Test `collect-numbers` against the examples:
+(collect-numbers "1abc2")
+(collect-numbers "pqr3stu8vwx")
+(collect-numbers "a1b2c3d4e5f")
+(collect-numbers "treb7uchet")
+
+;;
 (defn part-1
   [input]
-  (println "Part 1"))
+  (->> input
+       (map collect-numbers)
+       (apply +)))
 
 ;; Which gives our answer
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
